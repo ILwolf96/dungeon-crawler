571a38e96645128a5cbf5c279c5e5c455beb5f75
ILwolf96 <84623173+ILwolf96@users.noreply.github.com>
2026-08-24 23:30:11 +0300

    Stage 1

---
+++ dungeon-crawler/dungeon-crawler.cpp
@@ -0,0 +1,12 @@
+﻿// dungeon-crawler.cpp : Defines the entry point for the application.
+//
+
+#include "dungeon-crawler.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
