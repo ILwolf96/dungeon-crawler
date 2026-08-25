237be9de2cc7b08b54a9ff1502644b894d93892e
ILwolf96 <84623173+ILwolf96@users.noreply.github.com>
2026-08-25 17:49:53 +0300

    raylib is WORKING!
    I can confirm that the raylib is finally working proprly.
    only took me two days and 3 cans of Monster to figure it out :p

--- dungeon-crawler/dungeon-crawler.cpp
+++ dungeon-crawler/dungeon-crawler.cpp
@@ -1,12 +1,34 @@
-﻿// dungeon-crawler.cpp : Defines the entry point for the application.
-//
-
-#include "dungeon-crawler.h"
-
-using namespace std;
+﻿#include "raylib.h"
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+    InitWindow(640, 360, "Dungeon Crawler - raylib Test");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText(
+            "raylib is working!",
+            20,
+            20,
+            30,
+            DARKGRAY);
+
+        DrawRectangle(
+            270,
+            150,
+            100,
+            60,
+            MAROON);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
