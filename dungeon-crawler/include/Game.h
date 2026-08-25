8ffc326d323455490292fb39e4bbcc2f5a6dfdaf
ILwolf96 <84623173+ILwolf96@users.noreply.github.com>
2026-08-26 01:15:49 +0300

    preping Game

--- dungeon-crawler/include/Game.h
+++ dungeon-crawler/include/Game.h
@@ -1,12 +1,25 @@
 #pragma once
 
+#include "Map.h"
+#include "Player.h"
+
 namespace dungeon
 {
     class Game
     {
     public:
-        Game() = default;
+        Game();
 
         void update();
+
+        [[nodiscard]]
+        const Map& map() const noexcept;
+
+        [[nodiscard]]
+        const Player& player() const noexcept;
+
+    private:
+        Map m_map;
+        Player m_player;
     };
 }
--- dungeon-crawler/src/Game.cpp
+++ dungeon-crawler/src/Game.cpp
@@ -2,8 +2,24 @@
 
 namespace dungeon
 {
+    Game::Game()
+        : m_map(),
+        m_player(1, 1)
+    {
+    }
+
     void Game::update()
     {
         // Game-state updates will be implemented here.
+    }
+
+    const Map& Game::map() const noexcept
+    {
+        return m_map;
+    }
+
+    const Player& Game::player() const noexcept
+    {
+        return m_player;
     }
 }
