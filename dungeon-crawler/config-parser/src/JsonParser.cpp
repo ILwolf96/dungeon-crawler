f3becc17588588ab9e922ba2ee0b15e50d41e259
ILwolf96 <84623173+ILwolf96@users.noreply.github.com>
2026-08-27 21:31:35 +0300

    added nlohmann_json
    After few hours of research I found that there is the neat thing of nlohmann_json,
    this could allow me to have json == ini.

--- dungeon-crawler/config-parser/src/JsonParser.cpp
+++ dungeon-crawler/config-parser/src/JsonParser.cpp
@@ -1,12 +1,80 @@
 #include "config/JsonParser.h"
 
+#include <nlohmann/json.hpp>
+
+#include <fstream>
 #include <stdexcept>
+#include <string>
 
 namespace config
 {
-    ConfigData JsonParser::parse(std::string_view /*filePath*/) const
+    ConfigData JsonParser::parse(std::string_view filePath) const
     {
-        throw std::runtime_error(
-            "JSON parsing is not implemented yet.");
+        std::ifstream file{ std::string(filePath) };
+
+        if (!file.is_open())
+        {
+            throw std::runtime_error(
+                "Unable to open configuration file: " +
+                std::string(filePath));
+        }
+
+        try
+        {
+            const nlohmann::json jsonData = nlohmann::json::parse(file);
+
+            if (!jsonData.is_object())
+            {
+                throw std::runtime_error(
+                    "JSON configuration root must be an object.");
+            }
+
+            ConfigData data;
+
+            for (const auto& [sectionName, sectionData] : jsonData.items())
+            {
+                if (!sectionData.is_object())
+                {
+                    throw std::runtime_error(
+                        "JSON section must be an object: " +
+                        sectionName);
+                }
+
+                for (const auto& [key, value] : sectionData.items())
+                {
+                    if (value.is_string())
+                    {
+                        data.set(
+                            sectionName,
+                            key,
+                            value.get<std::string>());
+                    }
+                    else if (value.is_number() ||
+                        value.is_boolean())
+                    {
+                        data.set(
+                            sectionName,
+                            key,
+                            value.dump());
+                    }
+                    else
+                    {
+                        throw std::runtime_error(
+                            "Unsupported JSON value for " +
+                            sectionName + "." + key);
+                    }
+                }
+            }
+
+            return data;
+        }
+        catch (const nlohmann::json::parse_error& error)
+        {
+            throw std::runtime_error(
+                "Malformed JSON configuration '" +
+                std::string(filePath) +
+                "': " +
+                error.what());
+        }
     }
 }
