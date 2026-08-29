f7825dfb282b46b7febaa6a96e700d1d05db1157
ILwolf96 <84623173+ILwolf96@users.noreply.github.com>
2026-08-29 20:16:17 +0300

    Initial Combat Setup
    I have decided on what kind of Combat I want for the game to have, so thhis is the Initial commit for the upcoming Combat

---
+++ dungeon-crawler/include/CombatStats.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace dungeon
+{
+    struct CombatStats
+    {
+        int attacks{ 0 };
+        int precision{ 0 };
+        int strength{ 0 };
+        int toughness{ 0 };
+        int defense{ 0 };
+    };
+}
---
+++ dungeon-crawler/include/CombatTarget.h
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace dungeon
+{
+    class CombatTarget
+    {
+    public:
+        virtual ~CombatTarget() = default;
+
+        [[nodiscard]]
+        virtual int x() const noexcept = 0;
+
+        [[nodiscard]]
+        virtual int y() const noexcept = 0;
+
+        virtual void setPosition(int x, int y) = 0;
+
+        [[nodiscard]]
+        virtual int currentHp() const noexcept = 0;
+
+        [[nodiscard]]
+        virtual int maxHp() const noexcept = 0;
+
+        virtual void takeDamage(int amount) = 0;
+
+        [[nodiscard]]
+        virtual bool isDefeated() const noexcept = 0;
+
+        [[nodiscard]]
+        virtual const char* targetType() const noexcept = 0;
+    };
+}
